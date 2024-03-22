9f1500d03467802ea9a28cf99109fb37a7b0253f
Ceren <ceren1481@hotmail.com>
2024-03-22 04:40:01 +0300

    Changes are made.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,35 +1,33 @@
 import com.useInsider.Utility;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import static io.github.bonigarcia.wdm.WebDriverManager.isDockerAvailable;
 
 
 public class BaseTest {
     WebDriver webDriver;
     By acceptCookieBy = By.id("wt-cli-accept-all-btn");
-    WebDriverManager wdm = WebDriverManager.chromedriver().browserInDocker().dockerDefaultArgs("--disable-gpu,--no-sandbox").browserVersion("dev");
 
     @BeforeMethod
     public void startUp() throws InterruptedException {
+        WebDriverManager.chromedriver().driverVersion("125.0.6368.2").clearDriverCache().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         //System.setProperty("webdriver.chrome.driver", "/Users/ceren/Downloads/chromedriver-mac-arm64/chromedriver");
+        webDriver = new ChromeDriver(options);
         webDriver.manage().window().maximize();
         webDriver.get("https://useinsider.com/");
         Thread.sleep(2000);
         WebElement acceptCookie = webDriver.findElement(acceptCookieBy);
         acceptCookie.click();
-    }
-    @BeforeEach
-    void setupTest() {
-        Assert.assertTrue(isDockerAvailable());
-        webDriver = wdm.create();
     }
 
     @AfterMethod
@@ -39,3 +37,4 @@ public class BaseTest {
     }
 
 }
+
