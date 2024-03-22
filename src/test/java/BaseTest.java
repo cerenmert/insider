5fb560e1ee9d49f049b348091ce2b4098b9eb0c3
Ceren <ceren1481@hotmail.com>
2024-03-22 04:21:02 +0300

    Changes are made.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,10 @@
 import com.useInsider.Utility;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -13,17 +15,21 @@ import static io.github.bonigarcia.wdm.WebDriverManager.isDockerAvailable;
 public class BaseTest {
     WebDriver webDriver;
     By acceptCookieBy = By.id("wt-cli-accept-all-btn");
+    WebDriverManager wdm = WebDriverManager.chromedriver().browserInDocker().dockerDefaultArgs("--disable-gpu,--no-sandbox").browserVersion("dev");
 
     @BeforeMethod
     public void startUp() throws InterruptedException {
-        WebDriverManager wdm = WebDriverManager.chromedriver().browserInDocker().dockerDefaultArgs("--disable-gpu,--no-sandbox").browserVersion("beta");
-        webDriver = wdm.create();
         //System.setProperty("webdriver.chrome.driver", "/Users/ceren/Downloads/chromedriver-mac-arm64/chromedriver");
         webDriver.manage().window().maximize();
         webDriver.get("https://useinsider.com/");
         Thread.sleep(2000);
         WebElement acceptCookie = webDriver.findElement(acceptCookieBy);
         acceptCookie.click();
+    }
+    @BeforeEach
+    void setupTest() {
+        Assert.assertTrue(isDockerAvailable());
+        webDriver = wdm.create();
     }
 
     @AfterMethod
