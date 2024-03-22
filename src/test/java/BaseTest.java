5c9f9baa923a63184e889b5b0b9982ffeb1c6c28
Ceren <ceren1481@hotmail.com>
2024-03-22 03:48:33 +0300

    Changes are made.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,7 @@
 import com.useInsider.Utility;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.chainsaw.Main;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -15,6 +17,7 @@ public class BaseTest {
     @BeforeMethod
     public void startUp() throws InterruptedException {
         WebDriverManager.chromedriver().setup(); //bonigarcia
+        Main.main(new String[] { "standalone", "--port", "4445" });
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
         options.addArguments("--headless");
@@ -28,6 +31,10 @@ public class BaseTest {
         Thread.sleep(2000);
         WebElement acceptCookie = webDriver.findElement(acceptCookieBy);
         acceptCookie.click();
+    }
+    @BeforeEach
+    void setupTest() {
+        webDriver = WebDriverManager.chromedriver().remoteAddress("http://localhost:4445/wd/hub").create();
     }
 
     @AfterMethod
